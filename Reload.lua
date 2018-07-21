bb52f8e4eb13ae8cf3d09d5642b62a4941e45d38
Synse <Synse@users.noreply.github.com>
2018-07-21 15:57:43 -0500

    Initial commit

---
+++ Reload.lua
@@ -0,0 +1,11 @@
+-- slash commands
+SlashCmdList["RELOAD_RELOADUI"] = function()
+    Reload_ReloadUI();
+end
+SLASH_RELOAD_RELOADUI1 = "/reload";
+SLASH_RELOAD_RELOADUI2 = "/reloadui";
+SLASH_RELOAD_RELOADUI3 = "/rl";
+
+function Reload_ReloadUI()
+	ReloadUI();
+end
